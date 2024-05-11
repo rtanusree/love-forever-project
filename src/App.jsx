a143a3b74b0087e05f6e5572ea561feb27f6d939
rtanusree <rtanusree902@gmail.com>
2024-05-11 19:31:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Section1 from './section1'
+import About from  './component/about'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+
+function App() {
+  return(
+    <>
+    <Router>
+    <Routes>
+    <Route path="/"element={<Section1/>}>
+     </Route>
+     <Route path="/about"element={<About/>}>
+     </Route>
+    </Routes>
+  </Router>
+    </>
+  )
+}
+
+export default App
